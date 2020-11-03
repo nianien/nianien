2c472eebbbeaff789c1b8b5bc2ae86564ed2b31c
tengzhe.ln <tengzhe.ln@alibaba-inc.com>
2020-11-03 11:02:05 +0800

    函数工具类

--- src/main/java/com/nianien/core/functions/F.java
+++ src/main/java/com/nianien/core/functions/F.java
@@ -4,6 +4,7 @@ import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
 import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 /**
  * 构建Fluent API的Function工具<br/>
@@ -37,352 +38,364 @@ import java.util.function.Function;
  */
 public class F<T, U, V> {
 
-    /**
-     * 绑定对象
-     */
-    protected T target;
+  /**
+   * 绑定对象
+   */
+  protected T target;
 
-    /**
-     * 绑定的无参方法
-     */
-    protected Consumer<T> c1;
-    /**
-     * 含有单个参数的绑定方法
-     */
-    protected BiConsumer<T, U> c2;
-    /**
-     * 含有两个参数的绑定方法
-     */
-    protected TriConsumer<T, U, V> c3;
+  /**
+   * 绑定的无参方法
+   */
+  protected Consumer<T> c1;
+  /**
+   * 含有单个参数的绑定方法
+   */
+  protected BiConsumer<T, U> c2;
+  /**
+   * 含有两个参数的绑定方法
+   */
+  protected TriConsumer<T, U, V> c3;
 
-    /**
-     * 绑定的无参方法
-     */
-    protected Function<T, T> f1;
-    /**
-     * 含有单个参数的绑定方法
-     */
-    protected BiFunction<T, U, T> f2;
-    /**
-     * 含有两个参数的绑定方法
-     */
-    protected TriFunction<T, U, V, T> f3;
+  /**
+   * 绑定的无参方法
+   */
+  protected Function<T, T> f1;
+  /**
+   * 含有单个参数的绑定方法
+   */
+  protected BiFunction<T, U, T> f2;
+  /**
+   * 含有两个参数的绑定方法
+   */
+  protected TriFunction<T, U, V, T> f3;
 
-    /**
-     * 私有方法
-     *
-     * @param target
-     */
-    protected F(T target) {
-        this.target = target;
+  /**
+   * 私有方法
+   *
+   * @param target
+   */
+  protected F(T target) {
+    this.target = target;
+  }
+
+
+  /**
+   * 获取持有对象
+   *
+   * @return
+   */
+  public T $() {
+    return target;
+  }
+
+  /**
+   * 持有对象
+   *
+   * @param target
+   * @param <T>
+   * @return
+   */
+  public static <T, U, V> F<T, U, V> $(T target) {
+    return new F<>(target);
+  }
+
+  /**
+   * 执行函数，持有返回结果
+   *
+   * @param function
+   * @param <R>      函数返回类型
+   * @return
+   */
+  public <R> F<R, U, V> $(Function<T, R> function) {
+    F<R, U, V> f = (F<R, U, V>) this;
+    if (target != null) {
+      f.target = function.apply(target);
     }
+    return f;
+  }
 
-
-    /**
-     * 获取持有对象
-     *
-     * @return
-     */
-    public T $() {
-        return target;
+  /**
+   * 执行函数，参数为p，持有返回结果
+   *
+   * @param function
+   * @param p        函数参数
+   * @param <R>      函数返回类型
+   * @param <P>      参数类型
+   * @return
+   */
+  public <R, P> F<R, U, V> $(BiFunction<T, P, R> function, P p) {
+    F<R, U, V> f = (F<R, U, V>) this;
+    if (target != null) {
+      f.target = function.apply(target, p);
     }
+    return f;
+  }
 
-    /**
-     * 持有对象
-     *
-     * @param target
-     * @param <T>
-     * @return
-     */
-    public static <T, U, V> F<T, U, V> $(T target) {
-        return new F<>(target);
+  /**
+   * 执行函数，参数为p和q，持有返回结果
+   *
+   * @param function
+   * @param p        第一个参数
+   * @param q        第二个参数
+   * @param <R>      函数返回类型
+   * @param <P>      第一个参数类型
+   * @param <Q>      第二个参数类型
+   * @return
+   */
+  public <R, P, Q> F<R, U, V> $(TriFunction<T, P, Q, R> function, P p, Q q) {
+    F<R, U, V> f = (F<R, U, V>) this;
+    if (target != null) {
+      f.target = function.apply(target, p, q);
     }
+    return f;
+  }
 
-    /**
-     * 执行函数，持有返回结果
-     *
-     * @param function
-     * @param <R>      函数返回类型
-     * @return
-     */
-    public <R> F<R, U, V> $(Function<T, R> function) {
-        F<R, U, V> f = (F<R, U, V>) this;
-        if (target != null) {
-            f.target = function.apply(target);
-        }
-        return f;
+
+  /**
+   * 执行函数，忽略返回结果
+   *
+   * @param consumer
+   * @return
+   */
+  public F<T, U, V> $$(Consumer<T> consumer) {
+    if (target != null) {
+      consumer.accept(target);
     }
+    return this;
+  }
 
-    /**
-     * 执行函数，参数为p，持有返回结果
-     *
-     * @param function
-     * @param p        函数参数
-     * @param <R>      函数返回类型
-     * @param <P>      参数类型
-     * @return
-     */
-    public <R, P> F<R, U, V> $(BiFunction<T, P, R> function, P p) {
-        F<R, U, V> f = (F<R, U, V>) this;
-        if (target != null) {
-            f.target = function.apply(target, p);
-        }
-        return f;
+  /**
+   * 执行函数，参数为p，忽略返回结果
+   *
+   * @param consumer
+   * @param p
+   * @param <P>      参数类型
+   * @return
+   */
+  public <P> F<T, U, V> $$(BiConsumer<T, P> consumer, P p) {
+    if (target != null) {
+      consumer.accept(target, p);
     }
+    return this;
+  }
 
-    /**
-     * 执行函数，参数为p和q，持有返回结果
-     *
-     * @param function
-     * @param p        第一个参数
-     * @param q        第二个参数
-     * @param <R>      函数返回类型
-     * @param <P>      第一个参数类型
-     * @param <Q>      第二个参数类型
-     * @return
-     */
-    public <R, P, Q> F<R, U, V> $(TriFunction<T, P, Q, R> function, P p, Q q) {
-        F<R, U, V> f = (F<R, U, V>) this;
-        if (target != null) {
-            f.target = function.apply(target, p, q);
-        }
-        return f;
+
+  /**
+   * 执行函数，参数为p和q，忽略返回结果
+   *
+   * @param consumer
+   * @param p        第一个参数
+   * @param q        第二个参数
+   * @param <P>      第一个参数类型
+   * @param <P>      第二个参数类型
+   * @return
+   */
+  public <P, Q> F<T, U, V> $$(TriConsumer<T, P, Q> consumer, P p, Q q) {
+    if (target != null) {
+      consumer.accept(target, p, q);
     }
+    return this;
+  }
 
-
-    /**
-     * 执行函数，忽略返回结果
-     *
-     * @param consumer
-     * @return
-     */
-    public F<T, U, V> $$(Consumer<T> consumer) {
-        if (target != null) {
-            consumer.accept(target);
-        }
-        return this;
+  /**
+   * 当condition断言为真时,执行consumer
+   *
+   * @param condition
+   * @param consumer
+   */
+  public F<T, U, V> when(Predicate<T> condition, Consumer<T> consumer) {
+    if (condition.test(target)) {
+      consumer.accept(target);
     }
+    return this;
+  }
 
-    /**
-     * 执行函数，参数为p，忽略返回结果
-     *
-     * @param consumer
-     * @param p
-     * @param <P>      参数类型
-     * @return
-     */
-    public <P> F<T, U, V> $$(BiConsumer<T, P> consumer, P p) {
-        if (target != null) {
-            consumer.accept(target, p);
-        }
-        return this;
+  /**
+   * 绑定无参方法，执行{@link #accept()}进行方法调用
+   *
+   * @param consumer
+   * @return
+   */
+  public F<T, U, V> consumer(Consumer<T> consumer) {
+    this.c1 = consumer;
+    return this;
+  }
+
+
+  /**
+   * 绑定方法，执行{@link #accept(U)}进行方法调用
+   *
+   * @param consumer
+   * @return
+   */
+  public <U> F<T, U, V> consumer2(BiConsumer<T, U> consumer) {
+    F<T, U, V> f = (F<T, U, V>) this;
+    f.c2 = consumer;
+    return f;
+  }
+
+
+  /**
+   * 绑定方法，执行{@link #accept(U, V)}进行方法调用
+   *
+   * @param consumer
+   * @return
+   */
+  public <U, V> F<T, U, V> consumer3(TriConsumer<T, U, V> consumer) {
+    F<T, U, V> f = (F<T, U, V>) this;
+    f.c3 = consumer;
+    return f;
+  }
+
+  /**
+   * 绑定无参方法，执行{@link #apply()}进行方法调用
+   *
+   * @param function
+   * @return
+   */
+  public F<T, U, V> function(Function<T, T> function) {
+    this.f1 = function;
+    return this;
+  }
+
+  /**
+   * 绑定方法，执行{@link #apply(U)}进行方法调用
+   *
+   * @param function
+   * @param <U>      参数类型
+   * @return
+   */
+  public <U> F<T, U, V> function2(BiFunction<T, U, T> function) {
+    F<T, U, V> f = (F<T, U, V>) this;
+    f.f2 = function;
+    return f;
+  }
+
+  /**
+   * 绑定方法，执行{@link #apply(U, V)}进行方法调用
+   *
+   * @param function
+   * @param <U>      第一个参数类型
+   * @param <V>      第二个参数类型
+   * @return
+   */
+  public <U, V> F<T, U, V> function3(TriFunction<T, U, V, T> function) {
+    F<T, U, V> f = (F<T, U, V>) this;
+    f.f3 = function;
+    return f;
+  }
+
+
+  /**
+   * 调用无参方法，忽略方法执行结果
+   *
+   * @return
+   */
+  public F<T, U, V> accept() {
+    if (c1 != null) {
+      c1.accept(target);
     }
+    return this;
+  }
 
-
-    /**
-     * 执行函数，参数为p和q，忽略返回结果
-     *
-     * @param consumer
-     * @param p        第一个参数
-     * @param q        第二个参数
-     * @param <P>      第一个参数类型
-     * @param <P>      第二个参数类型
-     * @return
-     */
-    public <P, Q> F<T, U, V> $$(TriConsumer<T, P, Q> consumer, P p, Q q) {
-        if (target != null) {
-            consumer.accept(target, p, q);
-        }
-        return this;
+  /**
+   * 执行绑定方法，方法参数为u，忽略方法执行结果
+   *
+   * @param u 方法参数
+   * @return
+   */
+  public F<T, U, V> accept(U u) {
+    if (u instanceof Param) {
+      return accept((Param) u);
     }
-
-
-    /**
-     * 绑定无参方法，执行{@link #accept()}进行方法调用
-     *
-     * @param consumer
-     * @return
-     */
-    public F<T, U, V> consumer(Consumer<T> consumer) {
-        this.c1 = consumer;
-        return this;
+    if (c2 != null) {
+      c2.accept(target, u);
     }
+    return this;
+  }
 
 
-    /**
-     * 绑定方法，执行{@link #accept(U)}进行方法调用
-     *
-     * @param consumer
-     * @return
-     */
-    public <U> F<T, U, V> consumer2(BiConsumer<T, U> consumer) {
-        F<T, U, V> f = (F<T, U, V>) this;
-        f.c2 = consumer;
-        return f;
+  /**
+   * 执行绑定方法，方法参数为u和v，忽略方法执行结果
+   *
+   * @param u 第一个方法参数
+   * @param v 第二个方法参数
+   * @return
+   */
+  public F<T, U, V> accept(U u, V v) {
+    if (c3 != null) {
+      c3.accept(target, u, v);
     }
+    return this;
+  }
 
-
-    /**
-     * 绑定方法，执行{@link #accept(U, V)}进行方法调用
-     *
-     * @param consumer
-     * @return
-     */
-    public <U, V> F<T, U, V> consumer3(TriConsumer<T, U, V> consumer) {
-        F<T, U, V> f = (F<T, U, V>) this;
-        f.c3 = consumer;
-        return f;
+  /**
+   * 如果{@link Param#test()}为真，执行绑定方法，方法参数为{@link Param#get()}，忽略方法执行结果
+   *
+   * @param u 条件参数
+   * @return
+   */
+  public F<T, U, V> accept(Param<U> u) {
+    if (c2 != null && u.test()) {
+      c2.accept(target, u.get());
     }
+    return this;
+  }
 
-    /**
-     * 绑定无参方法，执行{@link #apply()}进行方法调用
-     *
-     * @param function
-     * @return
-     */
-    public F<T, U, V> function(Function<T, T> function) {
-        this.f1 = function;
-        return this;
+  /**
+   * 执行绑定的无参方法，持有方法执行结果
+   *
+   * @return
+   */
+  public F<T, U, V> apply() {
+    if (f1 != null) {
+      target = f1.apply(target);
     }
+    return this;
+  }
 
-    /**
-     * 绑定方法，执行{@link #apply(U)}进行方法调用
-     *
-     * @param function
-     * @param <U>      参数类型
-     * @return
-     */
-    public <U> F<T, U, V> function2(BiFunction<T, U, T> function) {
-        F<T, U, V> f = (F<T, U, V>) this;
-        f.f2 = function;
-        return f;
+
+  /**
+   * 执行绑定方法，方法参数为u，持有方法执行结果
+   *
+   * @param u 方法参数
+   * @return
+   */
+  public F<T, U, V> apply(U u) {
+    if (u instanceof Param) {
+      return apply((Param) u);
     }
-
-    /**
-     * 绑定方法，执行{@link #apply(U, V)}进行方法调用
-     *
-     * @param function
-     * @param <U>      第一个参数类型
-     * @param <V>      第二个参数类型
-     * @return
-     */
-    public <U, V> F<T, U, V> function3(TriFunction<T, U, V, T> function) {
-        F<T, U, V> f = (F<T, U, V>) this;
-        f.f3 = function;
-        return f;
+    if (f2 != null) {
+      target = f2.apply(target, u);
     }
+    return this;
+  }
 
 
-    /**
-     * 调用无参方法，忽略方法执行结果
-     *
-     * @return
-     */
-    public F<T, U, V> accept() {
-        if (c1 != null) {
-            c1.accept(target);
-        }
-        return this;
+  /**
+   * 执行绑定方法，方法参数为u和v，持有方法执行结果
+   *
+   * @param u 第一个方法参数
+   * @param v 第二个方法参数
+   * @return
+   */
+  public F<T, U, V> apply(U u, V v) {
+    if (f3 != null) {
+      target = f3.apply(target, u, v);
     }
+    return this;
+  }
 
-    /**
-     * 执行绑定方法，方法参数为u，忽略方法执行结果
-     *
-     * @param u 方法参数
-     * @return
-     */
-    public F<T, U, V> accept(U u) {
-        if (u instanceof Param) {
-            return accept((Param) u);
-        }
-        if (c2 != null) {
-            c2.accept(target, u);
-        }
-        return this;
+  /**
+   * 如果{@link Param#test()}为真，执行绑定方法，方法参数为{@link Param#get()}，持有方法执行结果
+   *
+   * @param u 条件参数
+   * @return
+   */
+  public F<T, U, V> apply(Param<U> u) {
+    if (f2 != null && u.test()) {
+      target = f2.apply(target, u.get());
     }
-
-
-    /**
-     * 执行绑定方法，方法参数为u和v，忽略方法执行结果
-     *
-     * @param u 第一个方法参数
-     * @param v 第二个方法参数
-     * @return
-     */
-    public F<T, U, V> accept(U u, V v) {
-        if (c3 != null) {
-            c3.accept(target, u, v);
-        }
-        return this;
-    }
-
-    /**
-     * 如果{@link Param#test()}为真，执行绑定方法，方法参数为{@link Param#get()}，忽略方法执行结果
-     *
-     * @param u 条件参数
-     * @return
-     */
-    public F<T, U, V> accept(Param<U> u) {
-        if (c2 != null && u.test()) {
-            c2.accept(target, u.get());
-        }
-        return this;
-    }
-
-    /**
-     * 执行绑定的无参方法，持有方法执行结果
-     *
-     * @return
-     */
-    public F<T, U, V> apply() {
-        if (f1 != null) {
-            target = f1.apply(target);
-        }
-        return this;
-    }
-
-
-    /**
-     * 执行绑定方法，方法参数为u，持有方法执行结果
-     *
-     * @param u 方法参数
-     * @return
-     */
-    public F<T, U, V> apply(U u) {
-        if (u instanceof Param) {
-            return apply((Param) u);
-        }
-        if (f2 != null) {
-            target = f2.apply(target, u);
-        }
-        return this;
-    }
-
-
-    /**
-     * 执行绑定方法，方法参数为u和v，持有方法执行结果
-     *
-     * @param u 第一个方法参数
-     * @param v 第二个方法参数
-     * @return
-     */
-    public F<T, U, V> apply(U u, V v) {
-        if (f3 != null) {
-            target = f3.apply(target, u, v);
-        }
-        return this;
-    }
-
-    /**
-     * 如果{@link Param#test()}为真，执行绑定方法，方法参数为{@link Param#get()}，持有方法执行结果
-     *
-     * @param u 条件参数
-     * @return
-     */
-    public F<T, U, V> apply(Param<U> u) {
-        if (f2 != null && u.test()) {
-            target = f2.apply(target, u.get());
-        }
-        return this;
-    }
+    return this;
+  }
 }
--- src/main/java/com/nianien/core/functions/Params.java
+++ src/main/java/com/nianien/core/functions/Params.java
@@ -1,8 +1,10 @@
 package com.nianien.core.functions;
 
+
 import com.nianien.core.util.ArrayUtils;
-import com.nianien.core.util.CollectionUtils;
 import com.nianien.core.util.StringUtils;
+
+import org.apache.commons.collections.CollectionUtils;
 
 import java.util.Collection;
 import java.util.Objects;
@@ -18,313 +20,314 @@ import java.util.function.Predicate;
  */
 public class Params {
 
+  /**
+   * 判断对象不为null
+   *
+   * @param obj
+   * @return
+   */
+  public static ImmutableParam<Object, Object> isNotNull(Object obj) {
+    return with(obj).when(Objects::nonNull);
+  }
+
+  /**
+   * 判断字符串不为空
+   *
+   * @param str
+   * @return
+   */
+  public static ImmutableParam<String, String> isNotEmpty(String str) {
+    return with(str, StringUtils::isNotEmpty);
+  }
+
+
+  /**
+   * 判断集合不为空
+   *
+   * @param collection
+   * @return
+   */
+  public static <T extends Collection<E>, E> ImmutableParam<T, T> isNotEmpty(T collection) {
+    return with(collection, CollectionUtils::isNotEmpty);
+  }
+
+
+  /**
+   * 判断数组不为空
+   *
+   * @param array
+   * @return
+   */
+  public static <T> Param<T[]> isNotEmpty(T[] array) {
+    return with(array, ArrayUtils::isNotEmpty);
+  }
+
+
+  /**
+   * 判断等于指定数值
+   *
+   * @param number
+   * @param other
+   * @return
+   */
+  public static ImmutableParam<Number, Number> eq(Number number, int other) {
+    return with(number).when(e -> e.intValue() == other);
+  }
+
+
+  /**
+   * 判断等于0
+   *
+   * @param number
+   * @return
+   */
+  public static ImmutableParam<Number, Number> eq0(Number number) {
+    return eq(number, 0);
+  }
+
+  /**
+   * 判断不等于指定数值
+   *
+   * @param number
+   * @param other
+   * @return
+   */
+  public static ImmutableParam<Number, Number> ne(Number number, int other) {
+    return with(number).when(e -> e.intValue() != other);
+  }
+
+
+  /**
+   * 判断不等于0
+   *
+   * @param number
+   * @return
+   */
+  public static ImmutableParam<Number, Number> ne0(Number number) {
+    return ne(number, 0);
+  }
+
+  /**
+   * 判断参数大于指定数值
+   *
+   * @param number
+   * @param other
+   * @return
+   */
+  public static ImmutableParam<Number, Number> gt(Number number, int other) {
+    return with(number).when(e -> e.intValue() > other);
+  }
+
+
+  /**
+   * 判断参数大于0
+   *
+   * @param number
+   * @return
+   */
+  public static ImmutableParam<Number, Number> gt0(Number number) {
+    return gt(number, 0);
+  }
+
+  /**
+   * 判断参数大于等于指定数值
+   *
+   * @param number
+   * @param other
+   * @return
+   */
+  public static ImmutableParam<Number, Number> ge(Number number, int other) {
+    return with(number).when(e -> e.intValue() >= other);
+  }
+
+  /**
+   * 判断参数大于等于指定数值
+   *
+   * @param number
+   * @return
+   */
+  public static ImmutableParam<Number, Number> ge0(Number number) {
+    return ge(number, 0);
+  }
+
+  /**
+   * 判断参数小于指定数值
+   *
+   * @param number
+   * @param other
+   * @return
+   */
+  public static ImmutableParam<Number, Number> lt(Number number, int other) {
+    return with(number).when(e -> e.intValue() < other);
+  }
+
+  /**
+   * 判断参数小于0
+   *
+   * @param number
+   * @return
+   */
+  public static ImmutableParam<Number, Number> lt0(Number number) {
+    return lt(number, 0);
+  }
+
+  /**
+   * 判断参数小于等于指定数值
+   *
+   * @param number
+   * @param other
+   * @return
+   */
+  public static ImmutableParam<Number, Number> le(Number number, int other) {
+    return with(number).when(e -> e.intValue() <= other);
+  }
+
+  /**
+   * 判断参数小于等于0
+   *
+   * @param number
+   * @return
+   */
+  public static ImmutableParam<Number, Number> le0(Number number) {
+    return le(number, 0);
+  }
+
+
+  /**
+   * 构建参数对象
+   *
+   * @param parameter
+   * @param <T>
+   * @return
+   */
+  public static <T> ImmutableParam<T, T> $(T parameter) {
+    return with(parameter);
+  }
+
+  /**
+   * 构建参数对象
+   *
+   * @param parameter 原始参数
+   * @param <T>
+   * @return
+   */
+  public static <T> ImmutableParam<T, T> with(T parameter) {
+    return new ImmutableParam<>(parameter, e -> true, Function.identity());
+  }
+
+  /**
+   * 构建参数对象,绑定条件断言
+   *
+   * @param parameter 原始参数
+   * @param predicate 条件断言
+   * @param <T>
+   * @return
+   */
+  public static <T> ImmutableParam<T, T> with(T parameter, Predicate<T> predicate) {
+    return new ImmutableParam<>(parameter, predicate, Function.identity());
+  }
+
+  /**
+   * 构建参数对象,绑定条件断言和参数转换函数
+   *
+   * @param parameter 原始参数
+   * @param predicate 条件断言
+   * @param function  参数转换函数
+   * @param <T>
+   * @param <P>
+   * @return
+   */
+  public static <T, P> ImmutableParam<T, P> with(P parameter,
+                                                 Predicate<P> predicate,
+                                                 Function<P, T> function) {
+    return new ImmutableParam<>(parameter, predicate, function);
+  }
+
+
+  /**
+   * 不变参数类型，通过条件断言和转换函数实现参数的校验和使用
+   *
+   * @param <T>
+   * @param <P>
+   */
+  public static class ImmutableParam<T, P> implements Param<T> {
+
     /**
-     * 判断对象不为null
-     *
-     * @param obj
-     * @return
+     * 初始参数
      */
-    public static Param<Object> isNotNull(Object obj) {
-        return with(obj).when(Objects::nonNull);
-    }
-
+    private P parameter;
     /**
-     * 判断字符串不为空
-     *
-     * @param str
-     * @return
+     * 条件断言,用于条件判定
      */
-    public static Param<String> isNotEmpty(String str) {
-        return with(str, StringUtils::isNotEmpty);
-    }
-
-
+    private Predicate<P> condition;
     /**
-     * 判断集合不为空
-     *
-     * @param collection
-     * @return
+     * 转换函数,用于参数处理
      */
-    public static <T extends Collection<E>, E> Param<T> isNotEmpty(T collection) {
-        return with(collection, CollectionUtils::isNotEmpty);
-    }
-
+    private Function<P, T> resolver;
 
     /**
-     * 判断数组不为空
-     *
-     * @param array
-     * @return
-     */
-    public static <T> Param<T[]> isNotEmpty(T[] array) {
-        return with(array, ArrayUtils::isNotEmpty);
-    }
-
-
-    /**
-     * 判断等于指定数值
-     *
-     * @param number
-     * @param other
-     * @return
-     */
-    public static Param<Number> eq(Number number, int other) {
-        return with(number).when(e -> e.intValue() == other);
-    }
-
-
-    /**
-     * 判断等于0
-     *
-     * @param number
-     * @return
-     */
-    public static Param<Number> eq0(Number number) {
-        return eq(number, 0);
-    }
-
-    /**
-     * 判断不等于指定数值
-     *
-     * @param number
-     * @param other
-     * @return
-     */
-    public static Param<Number> ne(Number number, int other) {
-        return with(number).when(e -> e.intValue() != other);
-    }
-
-
-    /**
-     * 判断不等于0
-     *
-     * @param number
-     * @return
-     */
-    public static Param<Number> ne0(Number number) {
-        return ne(number, 0);
-    }
-
-    /**
-     * 判断参数大于指定数值
-     *
-     * @param number
-     * @param other
-     * @return
-     */
-    public static Param<Number> gt(Number number, int other) {
-        return with(number).when(e -> e.intValue() > other);
-    }
-
-
-    /**
-     * 判断参数大于0
-     *
-     * @param number
-     * @return
-     */
-    public static Param<Number> gt0(Number number) {
-        return gt(number, 0);
-    }
-
-    /**
-     * 判断参数大于等于指定数值
-     *
-     * @param number
-     * @param other
-     * @return
-     */
-    public static Param<Number> ge(Number number, int other) {
-        return with(number).when(e -> e.intValue() >= other);
-    }
-
-    /**
-     * 判断参数大于等于指定数值
-     *
-     * @param number
-     * @return
-     */
-    public static Param<Number> ge0(Number number) {
-        return ge(number, 0);
-    }
-
-    /**
-     * 判断参数小于指定数值
-     *
-     * @param number
-     * @param other
-     * @return
-     */
-    public static Param<Number> lt(Number number, int other) {
-        return with(number).when(e -> e.intValue() < other);
-    }
-
-    /**
-     * 判断参数小于0
-     *
-     * @param number
-     * @return
-     */
-    public static Param<Number> lt0(Number number) {
-        return lt(number, 0);
-    }
-
-    /**
-     * 判断参数小于等于指定数值
-     *
-     * @param number
-     * @param other
-     * @return
-     */
-    public static Param<Number> le(Number number, int other) {
-        return with(number).when(e -> e.intValue() <= other);
-    }
-
-    /**
-     * 判断参数小于等于0
-     *
-     * @param number
-     * @return
-     */
-    public static Param<Number> le0(Number number) {
-        return le(number, 0);
-    }
-
-
-    /**
-     * 构建参数对象
-     *
-     * @param parameter
-     * @param <T>
-     * @return
-     */
-    public static <T> ImmutableParam<T, T> $(T parameter) {
-        return with(parameter);
-    }
-
-    /**
-     * 构建参数对象
-     *
      * @param parameter 原始参数
-     * @param <T>
-     * @return
+     * @param condition 条件断言
+     * @param resolver  转换函数
      */
-    public static <T> ImmutableParam<T, T> with(T parameter) {
-        return new ImmutableParam<>(parameter, e -> true, Function.identity());
+    public ImmutableParam(P parameter, Predicate<P> condition, Function<P, T> resolver) {
+      this.parameter = parameter;
+      this.condition = condition;
+      this.resolver = resolver;
     }
 
     /**
-     * 构建参数对象,绑定条件断言
+     * 绑定指定条件
      *
-     * @param parameter 原始参数
-     * @param predicate 条件断言
-     * @param <T>
+     * @param condition
      * @return
      */
-    public static <T> ImmutableParam<T, T> with(T parameter, Predicate<T> predicate) {
-        return new ImmutableParam<>(parameter, predicate, Function.identity());
+    public ImmutableParam<P, P> when(Predicate<P> condition) {
+      return new ImmutableParam<>(parameter, condition, Function.identity());
     }
 
+
     /**
-     * 构建参数对象,绑定条件断言和参数转换函数
+     * 将当前条件取反
      *
-     * @param parameter 原始参数
-     * @param predicate 条件断言
-     * @param function  参数转换函数
-     * @param <T>
-     * @param <P>
      * @return
      */
-    public static <T, P> ImmutableParam<T, P> with(P parameter,
-                                                   Predicate<P> predicate,
-                                                   Function<P, T> function) {
-        return new ImmutableParam<>(parameter, predicate, function);
+    public ImmutableParam<T, P> negate() {
+      return new ImmutableParam<>(parameter, condition.negate(), resolver);
     }
-
 
     /**
-     * 不变参数类型，通过条件断言和转换函数实现参数的校验和使用
+     * 绑定转换函数
+     *
+     * @param resolver
      * @param <T>
-     * @param <P>
+     * @return
      */
-    public static class ImmutableParam<T, P> implements Param<T> {
-
-        /**
-         * 初始参数
-         */
-        private P parameter;
-        /**
-         * 条件断言,用于条件判定
-         */
-        private Predicate<P> condition;
-        /**
-         * 转换函数,用于参数处理
-         */
-        private Function<P, T> resolver;
-
-        /**
-         * @param parameter 原始参数
-         * @param condition 条件断言
-         * @param resolver  转换函数
-         */
-        public ImmutableParam(P parameter, Predicate<P> condition, Function<P, T> resolver) {
-            this.parameter = parameter;
-            this.condition = condition;
-            this.resolver = resolver;
-        }
-
-        /**
-         * 绑定指定条件
-         *
-         * @param condition
-         * @return
-         */
-        public ImmutableParam<P, P> when(Predicate<P> condition) {
-            return new ImmutableParam<>(parameter, condition, Function.identity());
-        }
-
-
-        /**
-         * 将当前条件取反
-         *
-         * @return
-         */
-        public ImmutableParam<T, P> negate() {
-            return new ImmutableParam<>(parameter, condition.negate(), resolver);
-        }
-
-        /**
-         * 绑定转换函数
-         *
-         * @param resolver
-         * @param <T>
-         * @return
-         */
-        public <T> ImmutableParam<T, P> then(Function<P, T> resolver) {
-            return new ImmutableParam<>(parameter, condition, resolver);
-        }
-
-        /**
-         * 验证条件
-         *
-         * @return
-         */
-        @Override
-        public boolean test() {
-            return condition.test(parameter);
-        }
-
-        /**
-         * 返回处理后的参数
-         *
-         * @return
-         */
-        @Override
-        public T get() {
-            return resolver.apply(parameter);
-        }
-
-
+    public <T> ImmutableParam<T, P> then(Function<P, T> resolver) {
+      return new ImmutableParam<>(parameter, condition, resolver);
     }
+
+    /**
+     * 验证条件
+     *
+     * @return
+     */
+    @Override
+    public boolean test() {
+      return condition.test(parameter);
+    }
+
+    /**
+     * 返回处理后的参数
+     *
+     * @return
+     */
+    @Override
+    public T get() {
+      return resolver.apply(parameter);
+    }
+
+
+  }
 
 }
